a46e6f09cb4f72dfef09ea0c4ad67ce929d528fe
lcs-jdai <jdai@lcs.on.ca>
2021-10-26 14:17:34 -0400

    Initial Commit

---
+++ LayoutExcercise/LayoutExcerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutExcerciseApp.swift
+//  LayoutExcercise
+//
+//  Created by Jerry Dai on 2021-10-26.
+//
+
+import SwiftUI
+
+@main
+struct LayoutExcerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
